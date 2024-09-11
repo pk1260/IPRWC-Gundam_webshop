d0887d901dc0e8a10fa916a6ea0859ebd3663d00
pk1260 <pascalkuiper@live.nl>
2024-09-11 12:24:46 +0200

    Initial commit

---
+++ src/main/java/com/example/iprwcgundam_webshop/IprwcGundamWebshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.iprwcgundam_webshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IprwcGundamWebshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IprwcGundamWebshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/iprwcgundam_webshop/IprwcGundamWebshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.iprwcgundam_webshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IprwcGundamWebshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
